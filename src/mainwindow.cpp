3d75276ce0d4a8d2362e97b22a3bc927bcde58b2
gpushkarev <gpushkarev@solanteq.com>
2024-08-21 00:21:28 +0300

    Refactor and fixes

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,58 +2,24 @@
 // Created by nelos on 26.11.2023.
 //
 
-// You may need to build the project (run Qt uic code generator) to get "ui_MainWindow.h" resolved
-
 #include "mainwindow.h"
-#include "ui_MainWindow.h"
-#include <QZXing>
-#include <QUrl>
-#include <QTextStream>
-#include <QMimeData>
-#include <QMenu>
-#include <QStyle>
-#include <QGraphicsColorizeEffect>
-#include <QMessageBox>
+
+#include <iostream>
+#include <ostream>
 #include <QClipboard>
+#include <QGraphicsColorizeEffect>
+#include <QMenu>
+#include <QMimeData>
+#include <QStyle>
+#include <QUrl>
+#include <QString>
+#include <QZXing>
 
-MainWindow::MainWindow(QWidget *parent):
-        QMainWindow(parent), ui(new Ui::MainWindow) {
-    ui->setupUi(this);
-
-    setAcceptDrops(true);
-    setWindowFlag(Qt::MSWindowsFixedSizeDialogHint);
-    setBackgroundRole(QPalette::ColorRole::Dark);
-
-    auto effect = new QGraphicsColorizeEffect(this);
-    effect->setColor(Qt::red);
-    effect->setEnabled(false);
-    ui->fileNameText->setGraphicsEffect(effect);
-
-    ui->info->setOpenLinks(true);
-    ui->info->setOpenExternalLinks(true);
-
-    // trayIcon
-    trayIcon = new QSystemTrayIcon(this);
-    trayIcon->setIcon(this->style()->standardIcon(QStyle::SP_ComputerIcon));
-    trayIcon->setToolTip(QString::fromLocal8Bit("QR Decoder"));
-
-
-    auto *menu = new QMenu(this);
-    menu->addAction(QString::fromLocal8Bit("Exit"), this, SLOT(close()))
-            ->setIcon(this->style()->standardIcon(QStyle::SP_TitleBarCloseButton));
-
-    trayIcon->setContextMenu(menu);
-    trayIcon->showMessage(QString::fromLocal8Bit("QR Decoder"),
-                          QString::fromLocal8Bit("The application is minimized to the tray."
-                                                 "In order to expand the application window, click on the application icon in the tray"),
-                          QSystemTrayIcon::MessageIcon(QSystemTrayIcon::Information),
-                          100);
-    connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
-            this, SLOT(iconActivated(QSystemTrayIcon::ActivationReason)));
-
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
+    setupUi();
 }
 
-void MainWindow::closeEvent(QCloseEvent *event) {
+void MainWindow::closeEvent(QCloseEvent* event) {
     if (this->isVisible()) {
         event->ignore();
         this->hide();
@@ -64,7 +30,7 @@ void MainWindow::closeEvent(QCloseEvent *event) {
 }
 
 
-void MainWindow::iconActivated(QSystemTrayIcon::ActivationReason reason) {
+void MainWindow::iconActivated(const QSystemTrayIcon::ActivationReason reason) {
     switch (reason) {
         case QSystemTrayIcon::Trigger:
             if (!this->isVisible()) {
@@ -79,27 +45,33 @@ void MainWindow::iconActivated(QSystemTrayIcon::ActivationReason reason) {
     }
 }
 
-void MainWindow::showQRCode(const QString &file) {
+QString adjustFilePath(QString file) {
+    return file.remove(QString("file:///"));
+}
+
+void MainWindow::showQRCode(const QString& file) {
     if (!file.isEmpty()) {
-        ui->imageLabel->clear();
-        if (parseQr(QImage(file))) {
-            ui->fileNameText->setText(file);
+        imageLabel->clear();
+
+        QString filteredFilePath = adjustFilePath(file);
+        if (parseQr(QImage(filteredFilePath))) {
+            fileNameText->setText(filteredFilePath);
             return;
         }
     }
-    ui->fileNameText->setPlaceholderText(QString::fromLocal8Bit("Please specify path"));
-    ui->fileNameText->setFocus();
-    ui->fileNameText->graphicsEffect()->setEnabled(true);
+    fileNameText->setPlaceholderText(QString::fromLocal8Bit("Please specify path"));
+    fileNameText->setFocus();
+    fileNameText->graphicsEffect()->setEnabled(true);
 }
 
 void MainWindow::buttonClicked() {
-    showQRCode(ui->fileNameText->displayText());
+    showQRCode(fileNameText->displayText());
 }
 
-void MainWindow::dropEvent(QDropEvent *e) {
+void MainWindow::dropEvent(QDropEvent* e) {
     if (e->mimeData()->hasUrls()) {
         QString fileName = e->mimeData()->urls().first().toLocalFile();
-        ui->fileNameText->setText(fileName);
+        fileNameText->setText(fileName);
         showQRCode(fileName);
         e->acceptProposedAction();
     }
@@ -107,45 +79,40 @@ void MainWindow::dropEvent(QDropEvent *e) {
 }
 
 
-void MainWindow::dragEnterEvent(QDragEnterEvent *event) {
+void MainWindow::dragEnterEvent(QDragEnterEvent* event) {
     event->acceptProposedAction();
 }
 
-
-MainWindow::~MainWindow() {
-    delete ui;
-}
-
-bool MainWindow::parseQr(const QImage &image) {
+bool MainWindow::parseQr(const QImage& image) {
     if (image.isNull()) {
         return false;
     }
     clearEffects();
-    ui->imageLabel->setPixmap(QPixmap::fromImage(image));
+    imageLabel->setPixmap(QPixmap::fromImage(image));
 
 
     // QR code decoding
 
     QZXing decoder;
     decoder.setDecoder(
-            QZXing::DecoderFormat_QR_CODE |
-            QZXing::DecoderFormat_Aztec |
-            QZXing::DecoderFormat_CODABAR |
-            QZXing::DecoderFormat_CODE_39 |
-            QZXing::DecoderFormat_CODE_93 |
-            QZXing::DecoderFormat_CODE_128 |
-            QZXing::DecoderFormat_DATA_MATRIX |
-            QZXing::DecoderFormat_EAN_8 |
-            QZXing::DecoderFormat_EAN_13 |
-            QZXing::DecoderFormat_ITF |
-            QZXing::DecoderFormat_MAXICODE |
-            QZXing::DecoderFormat_PDF_417 |
-            QZXing::DecoderFormat_RSS_14 |
-            QZXing::DecoderFormat_RSS_EXPANDED |
-            QZXing::DecoderFormat_UPC_A |
-            QZXing::DecoderFormat_UPC_E |
-            QZXing::DecoderFormat_UPC_EAN_EXTENSION |
-            QZXing::DecoderFormat_CODE_128_GS1
+        QZXing::DecoderFormat_QR_CODE |
+        QZXing::DecoderFormat_Aztec |
+        QZXing::DecoderFormat_CODABAR |
+        QZXing::DecoderFormat_CODE_39 |
+        QZXing::DecoderFormat_CODE_93 |
+        QZXing::DecoderFormat_CODE_128 |
+        QZXing::DecoderFormat_DATA_MATRIX |
+        QZXing::DecoderFormat_EAN_8 |
+        QZXing::DecoderFormat_EAN_13 |
+        QZXing::DecoderFormat_ITF |
+        QZXing::DecoderFormat_MAXICODE |
+        QZXing::DecoderFormat_PDF_417 |
+        QZXing::DecoderFormat_RSS_14 |
+        QZXing::DecoderFormat_RSS_EXPANDED |
+        QZXing::DecoderFormat_UPC_A |
+        QZXing::DecoderFormat_UPC_E |
+        QZXing::DecoderFormat_UPC_EAN_EXTENSION |
+        QZXing::DecoderFormat_CODE_128_GS1
     );
     decoder.setTryHarder(true);
     decoder.setTryHarderBehaviour(QZXing::TryHarderBehaviour_ThoroughScanning | QZXing::TryHarderBehaviour_Rotate);
@@ -153,20 +120,20 @@ bool MainWindow::parseQr(const QImage &image) {
 
 
     if (!strQRCode.isEmpty()) {
-        ui->info->setMarkdown(strQRCode);
+        info->setMarkdown(strQRCode);
     } else {
-        ui->info->setTextColor(Qt::red);
-        ui->info->setText(QString::fromLocal8Bit("Failed to identify the QR code information!"));
+        info->setTextColor(Qt::red);
+        info->setText(QString::fromLocal8Bit("Failed to identify the QR code information!"));
     }
     return true;
 }
 
 void MainWindow::clearEffects() {
-    ui->fileNameText->graphicsEffect()->setEnabled(false);
+    fileNameText->graphicsEffect()->setEnabled(false);
 }
 
 
-void MainWindow::keyPressEvent(QKeyEvent *event) {
+void MainWindow::keyPressEvent(QKeyEvent* event) {
     if (event->matches(QKeySequence::Paste)) {
         paste();
     }
@@ -180,13 +147,18 @@ void MainWindow::keyPressEvent(QKeyEvent *event) {
 
 
 void MainWindow::paste() {
-    QClipboard *board = QApplication::clipboard();
-    auto image = board->image();
-    parseQr(image);
+    const QClipboard* board = QApplication::clipboard();
+    const auto image = board->image();
+
+    if (!image.isNull()) {
+        parseQr(image);
+    } else {
+        const auto path = board->text();
+        fileNameText->setText(path);
+        showQRCode(path);
+    }
 }
 
-void MainWindow::timerEvent(QTimerEvent *event) {
+void MainWindow::timerEvent(QTimerEvent* event) {
     QObject::timerEvent(event);
 }
-
-
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -5,62 +5,222 @@
 #ifndef QRDECODER_MAINWINDOW_H
 #define QRDECODER_MAINWINDOW_H
 
+#include <QGraphicsColorizeEffect>
+#include <QKeyEvent>
 #include <QMainWindow>
-#include <QDragEnterEvent>
+#include <QMenu>
+#include <QSpacerItem>
+#include <QString>
+#include <QStyle>
 #include <QSystemTrayIcon>
 #include <QWidget>
-#include <QString>
-#include <QCloseEvent>
-#include <QDropEvent>
-#include <QImage>
-#include <QKeyEvent>
-#include <QFile>
-#include <QAction>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+
+namespace Ui {
+class MainWindow;
+}
+
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow {
-Q_OBJECT
+        Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    public:
+        QAction* actionPaste{};
+        QWidget* widget{};
+        QWidget* gridLayoutWidget{};
+        QGridLayout* gridLayout{};
+        QPushButton* pushButton{};
+        QLineEdit* lineEdit{};
+        QLabel* imageLabel{};
+        QSpacerItem* horizontalSpacer{};
+        QSpacerItem* horizontalSpacer_2{};
+        QTextBrowser* info{};
+        QLineEdit* fileNameText{};
+        QSpacerItem* horizontalSpacer_3{};
 
-    void showQRCode(const QString &file);
+        void setupUi() {
+            if (objectName().isEmpty()) {
+                setObjectName(QString::fromUtf8("MainWindow"));
+            }
+            setWindowModality(Qt::ApplicationModal);
+            setTabletTracking(true);
+            setAutoFillBackground(true);
+            setAcceptDrops(true);
 
+            setWindowTitle(QCoreApplication::translate("MainWindow", "Qr Decoder", nullptr));
+            setWindowFlag(Qt::MSWindowsFixedSizeDialogHint);
+            setBackgroundRole(QPalette::ColorRole::Dark);
+            resize(500, 700);
+            QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+            sizePolicy.setHorizontalStretch(0);
+            sizePolicy.setVerticalStretch(0);
+            sizePolicy.setHeightForWidth(sizePolicy.hasHeightForWidth());
+            setSizePolicy(sizePolicy);
+            QFont font;
+            font.setPointSize(12);
+            setFont(font);
 
-    ~MainWindow() override;
+            // widget
+            widget = new QWidget(this);
+            widget->setObjectName(QString::fromUtf8("widget"));
+            widget->setSizePolicy(sizePolicy);
+            widget->setSizeIncrement(QSize(1, 1));
+            widget->setMouseTracking(true);
+            widget->setTabletTracking(false);
+            widget->setFocusPolicy(Qt::ClickFocus);
+            widget->setAcceptDrops(true);
+            widget->setAutoFillBackground(true);
+            setCentralWidget(widget);
+            gridLayoutWidget = new QWidget(widget);
+            gridLayoutWidget->setObjectName(QString::fromUtf8("gridLayoutWidget"));
+            gridLayoutWidget->setGeometry(QRect(0, 0, 500, 700));
+            gridLayout = new QGridLayout(gridLayoutWidget);
+            gridLayout->setSpacing(0);
+            gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+            gridLayout->setSizeConstraint(QLayout::SetDefaultConstraint);
+            gridLayout->setContentsMargins(0, 0, 0, 0);
 
-protected:
-    void closeEvent(QCloseEvent *event) override;
+            // actionPaste
+            actionPaste = new QAction(this);
+            actionPaste->setObjectName(QString::fromUtf8("actionPaste"));
+            actionPaste->setCheckable(true);
+            actionPaste->setAutoRepeat(false);
+            actionPaste->setText(QCoreApplication::translate("MainWindow", "Paste", nullptr));
+            actionPaste->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+V", nullptr));
 
-    void dropEvent(QDropEvent *event) override;
+            // lineEdit
+            lineEdit = new QLineEdit(gridLayoutWidget);
+            lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+            QSizePolicy sizePolicy1(QSizePolicy::Expanding, QSizePolicy::Fixed);
+            sizePolicy1.setHorizontalStretch(0);
+            sizePolicy1.setVerticalStretch(0);
+            sizePolicy1.setHeightForWidth(lineEdit->sizePolicy().hasHeightForWidth());
+            lineEdit->setSizePolicy(sizePolicy1);
+            lineEdit->setMinimumSize(QSize(500, 40));
+            QFont font1;
+            font1.setPointSize(16);
+            lineEdit->setFont(font1);
+            lineEdit->setFocusPolicy(Qt::NoFocus);
+            lineEdit->setAcceptDrops(false);
+            lineEdit->setAlignment(Qt::AlignCenter);
+            lineEdit->setReadOnly(true);
+            lineEdit->setText(QCoreApplication::translate("MainWindow", "Specify path or drag file", nullptr));
+            gridLayout->addWidget(lineEdit, 0, 0);
 
-    void dragEnterEvent(QDragEnterEvent *event) override;
+            // fileNameText
+            fileNameText = new QLineEdit(gridLayoutWidget);
+            fileNameText->setObjectName(QString::fromUtf8("fileNameText"));
+            QSizePolicy sizePolicy2(QSizePolicy::Expanding, QSizePolicy::Fixed);
+            sizePolicy2.setHorizontalStretch(1);
+            sizePolicy2.setVerticalStretch(0);
+            sizePolicy2.setHeightForWidth(fileNameText->sizePolicy().hasHeightForWidth());
+            fileNameText->setSizePolicy(sizePolicy2);
+            fileNameText->setMinimumSize(QSize(701, 31));
+            QFont font2;
+            font2.setPointSize(14);
+            fileNameText->setFont(font2);
+            fileNameText->setDragEnabled(true);
 
-private slots:
+            fileNameText->setText(QString());
+            fileNameText->setPlaceholderText(QCoreApplication::translate("MainWindow", "Specify Path", nullptr));
+            const auto effect = new QGraphicsColorizeEffect(this);
+            effect->setColor(Qt::red);
+            effect->setEnabled(false);
+            fileNameText->setGraphicsEffect(effect);
+            gridLayout->addWidget(fileNameText, 1, 0);
 
-    void paste();
+            // imageLabel
+            imageLabel = new QLabel(gridLayoutWidget);
+            imageLabel->setObjectName(QString::fromUtf8("imageLabel"));
+            imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+            imageLabel->setScaledContents(true);
+            imageLabel->setMinimumSize(QSize(500, 500));
+            imageLabel->setMaximumSize(QSize(500, 500));
+            gridLayout->addWidget(imageLabel, 2, 0);
 
-    void buttonClicked();
+            // info
+            info = new QTextBrowser(gridLayoutWidget);
+            info->setObjectName(QString::fromUtf8("info"));
+            info->setMinimumSize(QSize(0, 0));
+            info->setMaximumSize(QSize(16777215, 100));
+            info->setOpenLinks(true);
+            info->setOpenExternalLinks(true);
+            gridLayout->addWidget(info, 3, 0);
 
-    void clearEffects();
+            // pushButton
+            pushButton = new QPushButton(gridLayoutWidget);
+            pushButton->setObjectName(QString::fromUtf8("pushButton"));
+            pushButton->setText(QCoreApplication::translate("MainWindow", "Parse", nullptr));
 
-    void iconActivated(QSystemTrayIcon::ActivationReason reason);
+            gridLayout->addWidget(pushButton, 4, 0);
 
-protected:
-    void timerEvent(QTimerEvent *event) override;
+            // trayIcon
+            trayIcon = new QSystemTrayIcon(this);
+            trayIcon->setIcon(this->style()->standardIcon(QStyle::SP_ComputerIcon));
+            trayIcon->setToolTip(QString::fromLocal8Bit("QR Decoder"));
+            auto* menu = new QMenu(this);
+            menu->addAction(QString::fromLocal8Bit("Exit"), this, SLOT(close()))
+                ->setIcon(this->style()->standardIcon(QStyle::SP_TitleBarCloseButton));
 
-private:
+            trayIcon->setContextMenu(menu);
+            trayIcon->showMessage(QString::fromLocal8Bit("QR Decoder"),
+                                  QString::fromLocal8Bit(
+                                      "The application is minimized to the tray."
+                                      "In order to expand the application window, click on the application icon in the tray"),
+                                  QSystemTrayIcon::Information,
+                                  100);
 
-    Ui::MainWindow *ui;
-    QSystemTrayIcon *trayIcon;
+            connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+                    this, SLOT(iconActivated(QSystemTrayIcon::ActivationReason)));
+            connect(pushButton, SIGNAL(clicked()), this, SLOT(buttonClicked()));
+            connect(fileNameText, SIGNAL(textEdited(QString)), this, SLOT(clearEffects()));
 
-    bool parseQr(const QImage& image);
+            QMetaObject::connectSlotsByName(this);
+        }
 
-    void keyPressEvent(QKeyEvent *event) override;
+        explicit MainWindow(QWidget* parent = nullptr);
 
+        void showQRCode(const QString& file);
+
+        ~MainWindow() override = default;
+
+    protected:
+        void closeEvent(QCloseEvent* event) override;
+
+        void dropEvent(QDropEvent* event) override;
+
+        void dragEnterEvent(QDragEnterEvent* event) override;
+
+    private slots:
+        void paste();
+
+        void buttonClicked();
+
+        void clearEffects();
+
+        void iconActivated(QSystemTrayIcon::ActivationReason reason);
+
+    protected:
+        void timerEvent(QTimerEvent* event) override;
+
+    private:
+        QSystemTrayIcon* trayIcon;
+
+        bool parseQr(const QImage& image);
+
+        void keyPressEvent(QKeyEvent* event) override;
 };
 
 
